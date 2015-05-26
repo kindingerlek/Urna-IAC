dd3ae933e9ffac2e6325d11513677683f95f2379
kindingerlek <kindingerlek@hotmail.com>
2015-05-26 17:19:56 -0300

    view > login.php
    Começando a escrita do dicionário de IDs,  um documento de funções
    necessárias ao front-end. (E até ao back-end).

--- view/login.php
+++ view/login.php
@@ -9,28 +9,44 @@
     <link href="lib/css/style.css" rel="stylesheet">    
     </head>
   <body>
-    
+    <!--
+        Dicionário de IDs e Classes
+        
+        >IDs
+            - login-iptLogin        : Campo da seção 'Login' que receberá um CPF ou um AdminID.
+            - login-iptPassword     : Campo da seção 'Login' que receberá a senha do usuário;
+            - popup-pwRecover       : Identificação da seção 'Recuperar Senha'
+            - popup-newUser         : Identificação da seção 'Novo Usuário'
+            
+            - register-name         : Receberá o nome digitado;
+            - register-votingCard   : Receberá o título de eleitor;
+            - register-zone         : Receberá a zona do eleitor;
+            - register-session      : Receberá a seção do eleitor;
+        
+    -->  
+      
+          
     <!-- Conteúdo da página -->
     <div class="container page-content">
       <form id="form-login" class="middle" action="#">
         <h1 class="text-center">VOTE BEM</h1>
         
-        <!-- Input CPF -->
+        <!-- Input LOGIN -->
         <div class="form-group">
           <label>CPF:</label>
-          <input type="text" class="form-control" placeholder="Digite aqui seu CPF">
+          <input type="text" class="form-control" id="login-iptLogin" login-placeholder="Digite aqui seu CPF">
         </div>
         
         <!-- Input Senha -->
         <div class="form-group">
           <label for="exampleInputPassword3">Senha:</label>
-          <input type="password" class="form-control" id="exampleInputPassword3" placeholder="Digite aqui sua senha">
+          <input type="password" class="form-control" id="login-iptPassword" placeholder="Digite aqui sua senha">
         </div>
         
         <!-- Links -->
         <p class="text-right">
-          <a href="#" data-toggle="modal" data-target="#pwRecover">Esqueci minha senha.</a></br>
-          <a href="#" data-toggle="modal" data-target="#newUser">Primeiro Acesso?</a>
+          <a href="#" data-toggle="modal" data-target="#popup-pwRecover">Esqueci minha senha.</a></br>
+          <a href="#" data-toggle="modal" data-target="#popup-newUser">Primeiro Acesso?</a>
         </p>
         
         <!-- Submit -->
@@ -43,7 +59,7 @@
     
     
     <!-- Recuperação da Senha -->
-    <div class="modal fade" id="pwRecover" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal fade" id="popup-pwRecover" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <!-- Titulo -->
@@ -88,7 +104,7 @@
     </div>
     
     <!-- Novo Usuário -->
-    <div class="modal fade" id="newUser" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal fade" id="popup-newUser" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           
@@ -104,7 +120,7 @@
               <!-- Input Nome -->
               <div class="col-md-12 form-group">
                 <label>Nome:</label>
-                <input type="text" class="form-control" placeholder="Digite aqui seu Nome">
+                <input type="text" id="register-name" class="form-control" placeholder="Digite aqui seu Nome">
               </div>
               
               <!-- Inputs Eleitor -->
@@ -112,19 +128,19 @@
                 <!-- Input Título -->
                 <div class="col-md-8 form-group">
                   <label>Título de Eleitor:</label>
-                  <input type="text" class="form-control">
+                  <input type="text" id="register-votingCard" class="form-control">
                 </div>
                 
                 <!-- Input Zona-->
                 <div class="col-md-2 form-group">  
                   <label>Zona:</label>
-                  <input type="text" class="form-control" maxlength="2" >
+                  <input type="text" id="register-zone"class="form-control" maxlength="2" >
                 </div>
                 
                 <!-- Input Seção-->
                 <div class="col-md-2 form-group">  
                   <label>Seção:</label>
-                  <input type="text" class="form-control" maxlength="2" >
+                  <input type="text" id="register-session"class="form-control" maxlength="2" >
                 </div>  
               </div>
               
