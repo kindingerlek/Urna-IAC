770f86dfa96525d545bd994a2280810dcf1d8c26
Alissonkrul <alisson.krul@hotmail.com>
2015-05-27 17:06:42 -0300

    validations file model
    lololo

---
+++ model/validations.php
@@ -0,0 +1,30 @@
+<?php
+/* ARQUIVO \MODEL\VALIDATIONS.PHP */
+/* FUNÇÕES DE VALIDAÇÃO DE CAMPOS */
+/*  ULTIMA ALTERAÇÃO:26/05/2015   */
+
+/**/
+function evalCPF();
+
+/**/
+function validateUser(); 
+
+/**/
+function verifyId(); 
+
+/**/
+function verifyPW();
+
+/**/
+function evalDate(); 
+
+/**/
+function evalAge(); 
+
+/**/
+function evalEmail();
+
+/**/
+function checkPW(); 
+
+?>
