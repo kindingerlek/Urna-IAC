9c704e1f0dadddc65f4bca858f24a8604bca02f5
kindingerlek <kindingerlek@hotmail.com>
2015-05-27 16:28:12 -0300

    view > login.php
    Criei as IDs e Names das inputs

--- view/login.php
+++ view/login.php
@@ -10,9 +10,13 @@
     </head>
   <body>
     <!--
-        Dicionário de IDs e Classes
+        Dicionário de IDs, Names e Classes
         
-        >IDs
+        >Class
+            - mask-CPF              : Desenha a máscara de CPF;
+            
+        
+        >IDs e/ou Names
             - login-iptLogin        : Campo da seção 'Login' que receberá um CPF ou um AdminID.
             - login-iptPassword     : Campo da seção 'Login' que receberá a senha do usuário;
             - popup-pwRecover       : Identificação da seção 'Recuperar Senha'
@@ -22,7 +26,18 @@
             - register-votingCard   : Receberá o título de eleitor;
             - register-zone         : Receberá a zona do eleitor;
             - register-session      : Receberá a seção do eleitor;
-        
+            - register-cpf          : Receberá o CPF do eleitor;
+            - register-birthday     : Receberá a Data de Nascimento do Eleitor;
+            - register-codeZip      : Receberá o CEP do Usuário;
+            - register-adress       : Receberá o Endereço do Eleitor;
+            - register-adressNum    : Receberá o número do endereço do Eleitor;
+            - register-neighborhood : Receberá o Bairro do Eleitor;
+            - register-city         : Receberá a Cidade do Eleitor;
+            - register-state        : Receberá o estado do Eleitor;
+            - register-password     : Receberá a senha do Eleitor.
+            - register-cfmPassword  : Receberá novamente a senha
+            
+            - recover
     -->  
       
           
@@ -34,13 +49,13 @@
         <!-- Input LOGIN -->
         <div class="form-group">
           <label>CPF:</label>
-          <input type="text" class="form-control" id="login-iptLogin" login-placeholder="Digite aqui seu CPF">
+          <input type="text" class="form-control" id="login-iptLogin" name="login-iptLogin" placeholder="Digite aqui seu CPF">
         </div>
         
         <!-- Input Senha -->
         <div class="form-group">
           <label for="exampleInputPassword3">Senha:</label>
-          <input type="password" class="form-control" id="login-iptPassword" placeholder="Digite aqui sua senha">
+          <input type="password" class="form-control" id="login-iptPassword" name="login-iptPassword" placeholder="Digite aqui sua senha">
         </div>
         
         <!-- Links -->
@@ -74,7 +89,7 @@
                 <!-- Input CPF -->
                 <div class="col-md-12 form-group">
                   <label>CPF:</label>
-                  <input type="text" class="form-control" placeholder="Somente números">
+                  <input id="recover-cpf" name="recover-cpf" type="text" class="form-control" placeholder="Somente números">
                 </div>
                 
                 <!-- Input Código Enviado -->
@@ -82,7 +97,7 @@
                   <p class="text-right">Insira o código enviado para seu e-mail:</p>
                 </div>
                 <div class="col-md-2 form-group">
-                  <input type="text" class="form-control" maxLengh="4">
+                  <input type="text" id="" class="form-control" maxLengh="4">
                 </div>
                 <div class="col-md-12">
                   <button type="button" class="btn btn-primary btn-block">
@@ -120,7 +135,7 @@
               <!-- Input Nome -->
               <div class="col-md-12 form-group">
                 <label>Nome:</label>
-                <input type="text" id="register-name" class="form-control" placeholder="Digite aqui seu Nome">
+                <input type="text" id="register-name" name="register-name" class="form-control" placeholder="Digite aqui seu Nome">
               </div>
               
               <!-- Inputs Eleitor -->
@@ -128,19 +143,19 @@
                 <!-- Input Título -->
                 <div class="col-md-8 form-group">
                   <label>Título de Eleitor:</label>
-                  <input type="text" id="register-votingCard" class="form-control">
+                  <input type="text" id="register-votingCard" name="register-votingCard" class="form-control">
                 </div>
                 
                 <!-- Input Zona-->
                 <div class="col-md-2 form-group">  
                   <label>Zona:</label>
-                  <input type="text" id="register-zone"class="form-control" maxlength="2" >
+                  <input type="text" id="register-zone" name="register-zone" class="form-control" maxlength="2" >
                 </div>
                 
                 <!-- Input Seção-->
                 <div class="col-md-2 form-group">  
                   <label>Seção:</label>
-                  <input type="text" id="register-session"class="form-control" maxlength="2" >
+                  <input type="text" id="register-session" name="register-session" class="form-control" maxlength="2" >
                 </div>  
               </div>
               
@@ -148,13 +163,13 @@
                 <!-- Input CPF -->
                 <div class="col-md-6 form-group">
                   <label>CPF:</label>
-                  <input type="text" class="form-control" placeholder="Somente números">
+                  <input type="text" id="register-cpf" name="register-cpf" class="form-control" placeholder="Somente números">
                 </div>
                 
-                <!-- Input Estado -->
+                <!-- Input Data de Nascimento -->
                 <div class="col-md-6 form-group">
                   <label>Data de Nascimento:</label>
-                  <input type="text" class="form-control">
+                  <input type="text" id="register-birthday" name="register-birthday" name="register" class="form-control">
                 </div>
               </div>
               
@@ -163,20 +178,20 @@
                <!-- Input CEP -->
               <div class="col-md-12 form-group">
                 <label>CEP:</label>
-                <input type="text" class="form-control" placeholder="Somente números">
+                <input type="text" id="register-codeZip" name="register-codeZip" class="form-control" placeholder="Somente números">
               </div>
               
               <div>
                 <!-- Input Endereço -->
                 <div class="col-md-10 form-group">
                   <label>Endereço:</label>
-                  <input type="text" class="form-control">
+                  <input type="text" id="register-adress" name="register-adress" class="form-control">
                 </div>
                 
                 <!-- Input Número -->
                 <div class="col-md-2 form-group">
                   <label>Número:</label>
-                  <input type="text" class="form-control">
+                  <input type="text" id="register-adressNum" name="register-adressNum" class="form-control">
                 </div>
               </div>
               
@@ -184,38 +199,39 @@
                 <!-- Input Bairro -->
                 <div class="col-md-6 form-group">
                   <label>Bairro:</label>
-                  <input type="text" class="form-control">
+                  <input type="text" id="register-neighborhood" name="register-neighborhood" class="form-control">
                 </div>
                 
                 <!-- Input Cidade -->
                 <div class="col-md-4 form-group">
                   <label>Cidade:</label>
-                  <input type="text" class="form-control">
+                  <input type="text" id="register-city" name="register-city" class="form-control">
                 </div>
                 
                 <!-- Input Estado -->
                 <div class="col-md-2 form-group">
                   <label>Estado:</label>
-                  <input type="text" class="form-control" maxlenght="2">
+                  <input type="text" id="register-state" name="register-state" class="form-control" maxlenght="2">
                 </div>
               </div>
               
+              <!-- Input e-mail -->
               <div class="col-md-12 form-group">
                 <label>Email: <small>(válido, pois a recuperação de senha será através desse e-mail)</small></label>
-                <input type="email" class="form-control">
+                <input type="email" id="register-email" name="register-email" class="form-control">
               </div>
               
               <div>
                 <!-- Input Senha -->
                 <div class="col-md-6 form-group">
                   <label>Senha:</label>
-                  <input type="text" class="form-control" maxlenght="2">
+                  <input type="text" id="register-password" name="register-password" class="form-control" maxlenght="2">
                 </div>
                 
                 <!-- Input Confirmação da senha -->
                 <div class="col-md-6 form-group">
                   <label>Confirmar Senha:</label>
-                  <input type="text" class="form-control" maxlenght="2">
+                  <input type="text" id="register-cfmPassword" name="register-cfmPassword" class="form-control" maxlenght="2">
                 </div>
               </div>
               
