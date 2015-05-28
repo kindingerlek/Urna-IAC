1ce2285a7ead2c059683f9fd566bef09d0f33656
kindingerlek <kindingerlek@hotmail.com>
2015-05-28 15:04:07 -0300

    Atualização do jquery nas páginas

---
+++ view/admin_manager_voter.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    
+    <!-- Bootstrap core CSS -->
+    <link href="lib/css/bootstrap.css" rel="stylesheet">
+    <link href="lib/css/style.css" rel="stylesheet">    
+    
+    <script src="lib/js/jquery-1.11.0.js"></script>
+    <script src="lib/js/bootstrap.js"></script>
+      
+    <script type="text/javascript">
+    $(document).ready(function() {
+        $('dropdown-toggle').dropdown()
+    });
+    </script>
+    </head>
+  <body>
+    
+    <!-- Conteúdo da página -->
+    <div class="container page-content">
+      <div class="col-md-12">
+        <h1>Gerenciar Eleitores</h1>
+      </div>
+      
+      <form class="navbar-form" role="search">
+        <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+        </ul>
+        
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      
+      
+    </div>
+    
+  </body>
+</html>    
