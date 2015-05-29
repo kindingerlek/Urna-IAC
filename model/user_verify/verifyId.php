34f63f307df574d464b4992e8ec8a860c3419cbd
Alissonkrul <alisson.krul@hotmail.com>
2015-05-29 00:14:14 -0300

    Merge remote-tracking branch 'origin/development' into development
    Conflicts:
    	model/user_verify.php

---
+++ model/user_verify/verifyId.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once('verifyPw.php');
+require_once('openDb.php');
+require_once('error.php');
+require_once('openSession.php');
+
+/*
+* Título:
+*
+* Autor:Alisson
+* Data de Criação:28/05/2015
+*
+* Modificado por: Alisson
+* Data de Modificação:28/05/2015
+* 
+* Descrição: Recebe Id, verifica se é eleitor ou admin, se o registro existe no BD e se a senha está correta 
+*
+* Entrada: $_POST["id"], $_POST["senha"] 
+*
+* Saída: Valor númerico 0 se ID inválido, 1 Id válido de eleitor e 2 Id válido de administrador
+*
+* Valor de retorno: 0, 1 ou 2 
+*
+* Funções invocadas: verifyPw(), error(), openBd(), openSession()
+*
+*/
+
+function verifyId($id,$senha)
+{
+	//Verifica se é eleitor ou admin, ou seja verifica se existe o '#' no id
+
+	$isAdmin = $id[0] == "#" ? true : false;
+	
+	$pw = md5($pw);
+
+	//Busca no banco de dados o id informado
+	$connection = openBd();
+
+	$query = $isAdmin ? "SELECT *FROM USUARIOS WHERE idAdmin ='$id'" : "SELECT *FROM USUARIOS WHERE CPF ='$id'";
+	
+	$result = mysqli_query($connection, $query);
+
+	//Se der falha na busca encerra
+	if (!$result)
+	    {
+	    	error(4);
+	    	return 0;
+	    }
+	//Se não houver nehum registro encerra
+	if(!mysqli_num_rows($result))
+		{
+			error(3);
+			return 0;
+		}
+		
+	$userReg = mysqli_fetch_array($result);
+
+	mysqli_close($connection);
+	
+	//Compara senha com senha do BD
+	if($userReg["senha"] == $senha)   
+	{
+		openSession($userReg);
+
+		if($isAdmin)
+			return 2;
+		else
+			return 1;
+	}
+
+	return 0;
+}
+?>
