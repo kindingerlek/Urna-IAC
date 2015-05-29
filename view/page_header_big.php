e5cf8f000377c601e9a23c33a94d774e1838b30e
kindingerlek <kindingerlek@hotmail.com>
2015-05-29 17:15:41 -0300

    1.0.3.1

--- view/page_header_big.php
+++ view/page_header_big.php
@@ -1,5 +1,5 @@
 <!-- Cabeçalho da página -->  
-<div class="page-header">
+<div class="page-header-big">
     <img src="../resources/images/votebem_logo.png" id="header-logo"/>
     <h1>Vote Bem</h1>
     <small>Um simulador de votação brasileira</small>
--- view/page_header_sml.php
+++ view/page_header_sml.php
@@ -1,6 +1,16 @@
 <!-- Cabeçalho da página -->  
 <div class="page-header-sml">
+  <div style="float:left">
     <img src="../resources/images/votebem_logo_sml.png" id="header-logo"/>
     <h1>Vote Bem</h1>
+  </div>
+  
+  <div style="float:right">
+    <h2 id="page-title">Gerenciar</h2>
+    
+    <button type="button" class="btn btn-default" aria-label="Left Align">
+      <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span> Sair
+    </button>
+  </div>
 </div>
-<?php include "page_nav.php"  ?>
+
