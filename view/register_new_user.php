f33d03709c3233df5e3282b295dffed0f40b3197
kindingerlek <kindingerlek@hotmail.com>
2015-06-06 16:54:40 -0300

    Fix New User HTML

--- view/register_new_user.php
+++ view/register_new_user.php
@@ -14,127 +14,127 @@
         
           <div class="row">
             <!-- Input Nome -->
-            <div class="col-md-12 form-group">
+            <div class="col-md-12 form-group has-feedback">
               <label class="control-label">Nome:</label>
               <input type="text" id="register-name"  name="register-name" class="form-control" placeholder="Digite aqui seu Nome">
-              <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+              <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
             </div>
             
             <!-- Inputs Eleitor -->
             <div>
               <!-- Input Título -->
-              <div class="col-md-8 form-group">
+              <div class="col-md-8 form-group has-feedback">
                 <label>Título de Eleitor:</label>
                 <input type="text" id="register-votingCard"  name="register-votingCard" class="form-control">
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
               
               <!-- Input Zona-->
-              <div class="col-md-2 form-group">  
+              <div class="col-md-2 form-group has-feedback">  
                 <label>Zona:</label>
                 <input type="text" id="register-zone"   name="register-zone" class="form-control" maxlength="3" >
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
               
               <!-- Input Seção-->
-              <div class="col-md-2 form-group">  
+              <div class="col-md-2 form-group has-feedback">  
                 <label>Seção:</label>
                 <input type="text" id="register-session"   name="register-session" class="form-control" maxlength="4" >
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>  
             </div>
             
             <div>
               <!-- Input CPF -->
-              <div class="col-md-6 form-group">
+              <div class="col-md-6 form-group has-feedback">
                 <label>CPF:</label>
                 <input type="text" id="register-cpf"  name="register-cpf" class="form-control" placeholder="Somente números">
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
               
               <!-- Input Data de Nascimento -->
-              <div class="col-md-6 form-group">
+              <div class="col-md-6 form-group has-feedback">
                 <label>Data de Nascimento:</label>
                 <input type="text" id="register-birthday"  name="register-birthday" name="register" class="form-control">
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
             </div>
             
             
             
              <!-- Input CEP -->
-            <div class="col-md-12 form-group">
+            <div class="col-md-12 form-group has-feedback">
               <label>CEP:</label>
               <input type="text" id="register-zipCode"   name="register-zipCode" class="form-control" placeholder="Somente números">
-              <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+              <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
             </div>
             
             <div>
               <!-- Input Endereço -->
-              <div class="col-md-7 form-group">
+              <div class="col-md-7 form-group has-feedback">
                 <label>Endereço:</label>
                 <input type="text" id="register-address"  name="register-address" class="form-control">
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
               
               <!-- Input Número -->
-              <div class="col-md-2 form-group">
+              <div class="col-md-2 form-group has-feedback">
                 <label>Número:</label>
                 <input type="text" id="register-addressNum"  name="register-addressNum" class="form-control">
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
               
               <!-- Input Número -->
-              <div class="col-md-3 form-group">
+              <div class="col-md-3 form-group has-feedback">
                 <label>Complemento:</label>
                 <input type="text" id="register-complement"  name="register-complement" class="form-control">
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
             </div>
             
             <div>
               <!-- Input Bairro -->
-              <div class="col-md-6 form-group">
+              <div class="col-md-6 form-group has-feedback">
                 <label>Bairro:</label>
                 <input type="text" id="register-neighborhood"  name="register-neighborhood" class="form-control">
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
               
               <!-- Input Cidade -->
-              <div class="col-md-4 form-group">
+              <div class="col-md-4 form-group has-feedback">
                 <label>Cidade:</label>
                 <input type="text" id="register-city"  name="register-city" class="form-control">
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
               
               <!-- Input Estado -->
-              <div class="col-md-2 form-group">
+              <div class="col-md-2 form-group has-feedback">
                 <label>Estado:</label>
                 <input type="text" id="register-state"  name="register-state" class="form-control" maxlenght="2">
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
             </div>
             
             <!-- Input e-mail -->
-            <div class="col-md-12 form-group">
+            <div class="col-md-12 form-group has-feedback">
               <label>Email: <small>(válido, pois a recuperação de senha será através desse e-mail)</small></label>
               <input type="email" id="register-email"  name="register-email" class="form-control">
-              <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+              <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
             </div>
             
             <div>
               <!-- Input Senha -->
-              <div class="col-md-6 form-group">
+              <div class="col-md-6 form-group has-feedback">
                 <label>Senha:</label>
                 <input type="password" id="register-password"  name="register-password" class="form-control">
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
               
               <!-- Input Confirmação da senha -->
-              <div class="col-md-6 form-group">
+              <div class="col-md-6 form-group has-feedback">
                 <label>Confirmar Senha:</label>
                 <input type="password" id="register-cfmPassword"  name="register-cfmPassword" class="form-control">
-                <span class="glyphicon glyphicon-ok form-control-feedback" aria-hidden="true"></span>
+                <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
               </div>
             </div>
           </div>
