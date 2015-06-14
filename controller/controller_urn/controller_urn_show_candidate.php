07e422b4aef2c56e6726d21278c14984b3883eb5
cagrispan <cagrispan@gmail.com>
2015-06-13 22:04:37 -0300

    Zeca Diabo

--- controller/controller_urn/controller_urn_show_candidate.php
+++ controller/controller_urn/controller_urn_show_candidate.php
@@ -32,10 +32,10 @@ require_once($root.'model/verify/verify_candidate.php');
 
 $conn = openDB();
 
-$idCandidate["idCandidate"]= "24";
+$idCandidate["idCandidate"]= $_POST['candidate'];
 $idCandidate["idParty"]= substr($idCandidate["idCandidate"],0,2);
-$idCandidate["office"]= "PREFEITO";
-$idCandidate["idElection"]= "1";
+$idCandidate["office"]= "vereador";
+$idCandidate["idElection"]= "2";
 
 //$_POST["party"];
 //echo $idParty["idParty"];
@@ -46,7 +46,7 @@ if(verifyCandidate($idCandidate,$conn))
 	// Se existe partido
 	$candidate = verifycandidate($idCandidate,$conn); // pega no banco de dados o partido digitado
 	$candidate = mysqli_fetch_assoc($candidate);
-	echo("$('#urn-number').html('".$candidate["idPartido"]."');
+	echo("$('#urn-number').html('".$candidate["idCandidato"]."');
 		  $('#urn-candidateName').html('".$candidate["nomeFantasia"]."');
 		  $('#urn-candidatePhoto').css('background-image','url(".$candidate["foto"].")');" ); // IMPRIMIR DADOS NOS PADRÕES
 }else
