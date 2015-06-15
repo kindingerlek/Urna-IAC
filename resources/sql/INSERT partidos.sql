5f2db0f7542fe5a556f185c2a34f0cba2c6f0bda
kindingerlek <kindingerlek@hotmail.com>
2015-06-15 19:39:32 -0300

    Insert

--- resources/sql/INSERT partidos.sql
+++ resources/sql/INSERT partidos.sql
@@ -1,43 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: 14-Jun-2015 às 00:50
--- Versão do servidor: 5.6.17
--- PHP Version: 5.5.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `votebem`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `partidos`
---
-
-CREATE TABLE IF NOT EXISTS `partidos` (
-  `idPartido` varchar(2) NOT NULL DEFAULT '0',
-  `nome` varchar(200) DEFAULT NULL,
-  `sigla` varchar(10) DEFAULT NULL,
-  `logo` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`idPartido`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `partidos`
---
-
 INSERT INTO `partidos` (`idPartido`, `nome`, `sigla`, `logo`) VALUES
 ('10', 'PARTIDO REPUBLICANO BRASILEIRO', 'PRB', '../resources/party_logo/10.jpg'),
 ('11', 'PARTIDO PROGRESSISTA', 'PP','../resources/party_logo/11.jpg'),
@@ -71,7 +31,3 @@ INSERT INTO `partidos` (`idPartido`, `nome`, `sigla`, `logo`) VALUES
 ('70', 'PARTIDO TRABALHISTA DO BRASIL', 'PTdoB', '../resources/party_logo/70.jpg'),
 ('77', 'SOLIDARIEDADE', 'SD', '../resources/party_logo/77.jpg'),
 ('90', 'PARTIDO REPUBLICANO DA ORDEM SOCIAL', 'PROS', '../resources/party_logo/90.jpg');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
