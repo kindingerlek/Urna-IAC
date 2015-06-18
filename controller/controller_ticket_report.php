d50de4641bddd0149cb63a500caab5e80f81da11
Alissonkrul <alisson.krul@hotmail.com>
2015-06-17 21:26:24 -0300

    lol

--- controller/controller_ticket_report.php
+++ controller/controller_ticket_report.php
@@ -63,9 +63,10 @@ $votes = select('votos', 'idEleicao', $idElection, $conn); 			//Buscando tabela 
 
 
 $sql = "SELECT usuarios.*,eleicoes.*, ticket.data from ticket inner join usuarios on usuarios.cpf = ticket.cpf inner join eleicoes on eleicoes.idEleicao = '$idElection' WHERE ticket.idEleicao = eleicoes.idEleicao;";
-if(mysqli_query($conn, $sql))
+$tickets = mysqli_query($conn, $sql);
+if(mysqli_num_rows($tickets)>0)
 {
-    $tickets = mysqli_query($conn, $sql);
+    
 
     //Header Table
      
@@ -74,14 +75,9 @@ if(mysqli_query($conn, $sql))
     // $pdf->Cell(40,7,'oi',1,0); 
 
     //print_r($vagas);
-    $pdf->SetFont('Arial','',12);
-        
+        $pdf->SetFont('Arial','',12);
         $pdf->Cell(0,7,utf8_decode('CODIGO DA ELEIÇÃO: '.$idElection));
-
-
-        
         $pdf->Ln(15);    
-          
         $pdf->SetFont('Arial','B',8);
         $pdf->Cell(25,7,utf8_decode('INSCRIÇÃO'),1,0,'C');
         $pdf->Cell(70,7,'NOME DO ELEITOR',1,0,'C');    
@@ -103,11 +99,13 @@ if(mysqli_query($conn, $sql))
     	
 
     }
-    $pdf->Output();
 }else
 {
-    echo("alert('Nenhum dado nesta eleição!')");
+    $pdf->Ln(25);
+    $pdf->SetFont('Arial','',12);
+    $pdf->Cell(0,10,utf8_decode("NÃO EXISTEM DADOS NESSA ELEIÇÃO"),0,1,'C'); 
 }
+$pdf->Output();
 	//sql office
 	//query
 	//insere linha a linha em $table
