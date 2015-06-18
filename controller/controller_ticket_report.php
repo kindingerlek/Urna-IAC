52f0cc296e1faffa7cff7ed8896a4d80dceb94d3
kindingerlek <kindingerlek@hotmail.com>
2015-06-18 01:15:05 -0300

    PDF header footer template

--- controller/controller_ticket_report.php
+++ controller/controller_ticket_report.php
@@ -1,49 +1,12 @@
 
 
 <?php
+$title = 'RELATÓRIO DE ELEIÇÃO';
+
+require_once('../model/pdf_template/PDF.php'); 
 require_once('../model/open_db/open_db.php');
 require_once('../model/select/select.php');
 require_once('../model/error/error.php');
-require_once('../resources/fpdf/fpdf.php');
-
-class PDF extends FPDF
-{
-    // Page header
-    function Header()
-    {
-        // Logo
-        $this->Image('../resources/images/ufpr_logo.png',10,6,30);
-        // Arial bold 15
-        $this->SetFont('Arial','B',15);
-        // Move to the right
-        $this->Cell(50);
-        // Title
-        $this->Cell(110,10, utf8_decode('RELATÓRIO DE ELEITORES'),1,0,'C');
-        // Line break
-        $this->Ln(10);
-        
-        // Arial 14
-        $this->SetFont('Arial','',15);
-        // Move to the right
-        $this->Cell(50);
-        // Title
-        $this->Cell(110,8,'SIMULADOR VOTE BEM',1,0,'C');
-        
-        // Line break
-        $this->Ln(20);
-    }
-    
-    // Page footer
-    function Footer()
-    {
-        // Position at 1.5 cm from bottom
-        $this->SetY(-15);
-        // Arial italic 8
-        $this->SetFont('Arial','I',8);
-        // Page number
-        $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-    }
-}
 
 // Instanciation of inherited class
 $pdf = new PDF();
